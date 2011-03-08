8850f3009939a6828b421221ec281fcd0ea059e3
Predrag Knezevic <predrag.knezevic@abz-reporting.com>
2011-03-08 10:22:52 +0100

    removed dependsOn statement because controllers plugin is part of distribution anyway. Actually, we do not use the plugin, we just want to do our stuff after controllers plugin has been loaded.

--- TransactionalControllerGrailsPlugin.groovy
+++ TransactionalControllerGrailsPlugin.groovy
@@ -21,14 +21,13 @@ import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU
 
 class TransactionalControllerGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.1.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
-    def dependsOn = [controllers: "1.3.5 > *"]
     def observe = ['controllers']
     def loadAfter = ['controllers']
     
